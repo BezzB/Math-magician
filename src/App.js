f79b2bf8d5d0577be9855e5e6ae49adc429cd9d5
BezzB <118984781+BezzB@users.noreply.github.com>
2023-05-10 11:57:07 +0300

    Update component.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import App from './component/Calculator';
 
-function App() {
+function MainApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Math Magician
-        </a>
-      </header>
+    <div>
+      <h1>Calculator</h1>
+      <App />
     </div>
   );
 }
 
-export default App;
+export default MainApp;
---
+++ src/component/Calculator.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function App() {
+  const createDigits = () => {
+    const digits = [];
+    // eslint-disable-next-line no-plusplus
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        // eslint-disable-next-line react/button-has-type
+        <button key={i}>
+          {i}
+        </button>,
+      );
+    }
+    return digits;
+  };
+
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span>
+          0
+        </div>
+        <div className="operators">
+          <button type="button">/</button>
+          <button type="button">*</button>
+          <button type="button">+</button>
+          <button type="button">-</button>
+          <button type="button">DEL</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <button type="button">0</button>
+          <button type="button">.</button>
+          <button type="button">=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
