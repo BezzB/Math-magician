5695dfacf3f178173558aa59f81ee8c838cde2b8
BezzB <118984781+BezzB@users.noreply.github.com>
2023-05-11 20:06:58 +0300

    Update calculatro

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,29 +1,64 @@
-import React from 'react';
+/* eslint-disable no-console */
+import { useState } from 'react';
 import Digits from './digitshow';
+// import calculate from './logic/calculate';
 
-// Call the Operate function
 function Calculator() {
+  const [calc, setCalc] = useState('');
+  const [result, setResult] = useState('');
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === '')
+      || (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCalc((prevCalc) => prevCalc + value);
+
+    if (!ops.includes(value)) {
+      // eslint-disable-next-line no-eval
+      setResult(eval(calc + value).toString());
+    }
+  };
+
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span>
+          {result ? <span>{result}</span> : ''}
+          {calc || '0'}
           0
         </div>
         <div className="scienceo">
           <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
+          <button type="button" onClick={() => updateCalc('+/-')}>
+            +/-
+          </button>
+          <button type="button" onClick={() => updateCalc('%')}>
+            %
+          </button>
         </div>
         <div className="operators">
-          <button type="button">÷</button>
-          <button type="button">*</button>
-          <button type="button">+</button>
-          <button type="button">-</button>
+          <button type="button" onClick={() => updateCalc('÷')}>
+            ÷
+          </button>
+          <button type="button" onClick={() => updateCalc('*')}>
+            *
+          </button>
+          <button type="button" onClick={() => updateCalc('+')}>
+            +
+          </button>
+          <button type="button" onClick={() => updateCalc('-')}>
+            -
+          </button>
           <button type="button">=</button>
         </div>
         <div className="digits">
-          <Digits />
+          <Digits updateCalc={updateCalc} />
         </div>
       </div>
     </div>
