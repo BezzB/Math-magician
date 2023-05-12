166306b18eebf534561068175eb4b971c879f6aa
BezzB <118984781+BezzB@users.noreply.github.com>
2023-05-12 20:24:05 +0300

    Developed the quotes and the subsequent settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Calculator from './component/Calculator';
+import QuoteDisplay from './Quotes';
+import './QuotesDisplay.css';
 
 function App() {
   return (
-    <div>
+    <div className="homecontainer">
       <h1>Calculator</h1>
       <Calculator />
+      <QuoteDisplay />
     </div>
   );
 }
